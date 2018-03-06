e1035a021d7589239a151dc98f65acd27ccf9128
nmei-42 <nmei@e1z2r9p35.42.us.org>
2018-03-05 16:46:32 -0800

    FT_42_fourtytwo_S_H.h

--- includes/ft_42sh.h
+++ includes/ft_42sh.h
@@ -6,11 +6,51 @@
 /*   By: nmei <nmei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 14:08:33 by nmei              #+#    #+#             */
-/*   Updated: 2018/03/05 15:33:46 by nmei             ###   ########.fr       */
+/*   Updated: 2018/03/05 16:46:06 by nmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_42SH_H
 # define FT_42SH_H
+
+/*
+**	This will be the data structure that stores our environment variables
+**	Maybe other stuff too?
+*/
+
+typedef struct	s_env
+{
+
+}				t_env;	
+
+/*
+**	main.c
+*/
+
+void			sh_loop()
+
+/*
+**	env.c
+*/
+
+void			init_sh(char **envp);
+
+/*
+**	listen.c
+*/
+
+void			sh_listen();
+
+/*
+**	parse.c
+*/
+
+void			sh_parse();
+
+/*
+**	dispatcher.c
+*/
+
+void			sh_dispatcher();
 
 #endif
