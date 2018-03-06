8c889085741153a2b59d785f77c4af2d29df52f1
nmei-42 <nmei@e1z2r9p35.42.us.org>
2018-03-06 14:56:17 -0800

    adding more docs to hash function. Also adding a HASHMAP_SIZE define in our header

--- includes/ft_42sh.h
+++ includes/ft_42sh.h
@@ -6,12 +6,13 @@
 /*   By: nmei <nmei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 14:08:33 by nmei              #+#    #+#             */
-/*   Updated: 2018/03/06 14:21:15 by nmei             ###   ########.fr       */
+/*   Updated: 2018/03/06 14:53:24 by nmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_42SH_H
 # define FT_42SH_H
+# define HASHMAP_SIZE	512
 
 /*
 **	This will be the data structure that stores our environment variables
@@ -27,7 +28,7 @@ typedef struct			s_hash_node
 
 typedef struct			s_env
 {
-	t_hash_node			env_hashmap[256];
+	t_hash_node			env_hashmap[HASHMAP_SIZE];
 }						t_env;	
 
 /*
--- srcs/hashmap_utils.c
+++ srcs/hashmap_utils.c
@@ -6,7 +6,7 @@
 /*   By: nmei <nmei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 13:55:36 by nmei              #+#    #+#             */
-/*   Updated: 2018/03/06 14:37:46 by nmei             ###   ########.fr       */
+/*   Updated: 2018/03/06 14:53:31 by nmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,19 +45,26 @@ t_hash_node		*new_hash_node(char *key, char *val)
 **	hash()
 **	Uses the 'XOR' version of the djb2 hash.
 **	https://softwareengineering.stackexchange.com/questions/49550/
+**
+**	Certainly, not the *best*, but it's easy to implement...
+**
+**	I also remap the hash result to the size of our hashmap
+**	(this will lead to more collisions, but our hashmap will use
+**	less memory... tradeoffs...)
 */
 
-unsigned long	ash(char *str)
+unsigned long	djb2_hash(char *str)
 {
 	unsigned long	hash;
 	int				c;
 
+	hash = 5381;
 	while (c = *str++)
 		hash = ((hash << 5) + hash) ^ c;
-	return (hash % 256)
+	return (hash % HASHMAP_SIZE)
 }
 
-t_hash_node 	*get_hash_node(t_hash_node hashmap[256])
+t_hash_node 	*get_hash_node(t_hash_node hashmap[HASHMAP_SIZE])
 {
 
 }
