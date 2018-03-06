fe39d619ff024030f3fb0338beee22bb4a565571
nmei-42 <nmei@e1z2r9p35.42.us.org>
2018-03-06 15:42:03 -0800

    adding get_hash_node and remove_hash_node util functions

--- srcs/hashmap_utils.c
+++ srcs/hashmap_utils.c
@@ -6,7 +6,7 @@
 /*   By: nmei <nmei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 13:55:36 by nmei              #+#    #+#             */
-/*   Updated: 2018/03/06 14:53:31 by nmei             ###   ########.fr       */
+/*   Updated: 2018/03/06 15:41:13 by nmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,71 @@ unsigned long	djb2_hash(char *str)
 	return (hash % HASHMAP_SIZE)
 }
 
-t_hash_node 	*get_hash_node(t_hash_node hashmap[HASHMAP_SIZE])
-{
+/*
+**	get_hash_node()
+**
+**	Given a hashmap and a key, return the hashnode that contains the exact same
+**	key.
+**
+**	If get_hash_node() can't find a matching node, it returns NULL
+*/
 
+t_hash_node 	*get_hash_node(t_hash_node *hashmap, char *key)
+{
+	t_hash_node	*curr;
+
+	if (hashmap)
+	{
+		curr = hashmap[djb2_hash(key)];
+		while (curr)
+		{
+			if (ft_strcmp(curr->key, key) == 0)
+				return (curr);
+			curr = curr->next;
+		}
+	}
+	return (NULL);
+}
+
+/*
+**	remove_hash_node()
+**
+**	Given a hashmap and key, we want to remove (free) the hashnode that
+**	contains the corresponding key.
+**
+**	Our remove node function also needs to check if there are nodes
+**	before or after the one we want to remove. If so, we need to do some
+**	pointer swapping so that the 'prev' of curr will point to curr's 'next'.
+**
+**	If a hash node is sucessfully removed then the function returns 0.
+**	If a hash node wasn't found or the hashmap is invalid returns 1.
+*/
+
+int				remove_hash_node(t_hash_node *hashmap, char *key)
+{
+	t_hash_node *prev;
+	t_hash_node *curr;
+
+	if (hashmap)
+	{
+		prev = NULL;
+		curr = hashmap[djb2_hash(key)];
+		while (curr)
+		{
+			if (ft_strcmp(curr->key, key) == 0)
+			{
+				if (prev)
+					prev->next = curr->next;
+				if (curr->key)
+					free(curr->key);
+				if (curr->val)
+					free(curr->val);
+				free(curr);
+				return (0);
+			}
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+	return (1);
 }
