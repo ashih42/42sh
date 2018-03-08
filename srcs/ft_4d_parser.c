ecaa59b122959b3129bdc48d833e8d1ceda3706f
Andres Puel <andreshpuel@gmail.com>
2018-03-08 04:31:26 -0800

    4d parser outline written

--- srcs/ft_4d_parser.c
+++ srcs/ft_4d_parser.c
@@ -6,7 +6,7 @@
 /*   By: apuel <apuel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 18:49:13 by apuel             #+#    #+#             */
-/*   Updated: 2018/03/07 20:44:49 by apuel            ###   ########.fr       */
+/*   Updated: 2018/03/08 04:31:04 by apuel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,24 @@
 
 char	****ft_4d_parser(char *input)
 {
+	char	**groups;
+	char	**cmds;
+	char	****result;
+	size_t	i;
+	size_t	j;
+
+	groups = ft_strsplit(input, ';');
+	result = ft_memalloc((ft_char_array_length(groups) + 1) * sizeof(char ***));
+	i = 0;
+	while (groups[i])
+	{
+		cmds = //a form of ft_strsplit that splits between '|' and '&&' (shouldn't remove '|', leave it at the end of the left string)
+		result[i] = ft_memalloc((ft_char_array_length(cmds) + 1) * sizeof(char **));
+		j = 0;
+		while (cmds[j])
+			result[i][j] = split_argv(cmds[j], WHITESPACE);
+		ft_char_array_del(cmds);
+	}
+	ft_char_array_del(groups);
+	return (result);
 }
