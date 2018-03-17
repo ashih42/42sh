de878c7aefcaacf5b004a0f3476f23c9b5d6fed7
Andy <AndyShih51@gmail.com>
2018-03-17 06:09:21 -0700

    norminete partie sept : retour de la super usine

--- srcs/ft_super_factory.c
+++ srcs/ft_super_factory.c
@@ -5,7 +5,7 @@ static int		find_closing(char *s, int *open)
 	int	level;
 	int	i;
 	int	dummy;
-	
+
 	level = 0;
 	i = 0;
 	if (!open)
@@ -27,16 +27,7 @@ static int		find_closing(char *s, int *open)
 	}
 	return (-1);
 }
-/*
-char	*ft_substr(char *s, int start, int len)
-{
-	char *str;
 
-	str = ft_strnew(len);
-	ft_strncpy(str, s + start, len);
-	return (str);
-}
-*/
 static void		ft_strtrisect(char *s, char **head, char **tail, char **mid)
 {
 	int	close;
@@ -50,7 +41,7 @@ static void		ft_strtrisect(char *s, char **head, char **tail, char **mid)
 
 static t_list	*split_commas(char *s)
 {
-	t_list *list;
+	t_list	*list;
 	char	*head;
 	int		brackets;
 	char	*str;
@@ -60,11 +51,9 @@ static t_list	*split_commas(char *s)
 	head = s;
 	while (*s)
 	{
-		if (*s == '{' && (s == head || *(s - 1) != '\\'))
-			brackets++;
-		else if (*s == '}' && (s == head || *(s - 1) != '\\'))
-			brackets--;
-		else if (*s == ',' && brackets == 0 && (s == head || *(s - 1) != '\\'))
+		(*s == '{' && (s == head || *(s - 1) != '\\')) ? brackets++ : 0;
+		(*s == '}' && (s == head || *(s - 1) != '\\')) ? brackets-- : 0;
+		if (*s == ',' && brackets == 0 && (s == head || *(s - 1) != '\\'))
 		{
 			str = ft_strnew(s - head);
 			ft_strncpy(str, head, s - head);
@@ -81,61 +70,51 @@ static t_list	*split_commas(char *s)
 
 static void		ft_str_factory(char *s, t_list **list)
 {
-	char	*head;
-	char	*mid;
-	char	*tail;
-	t_list	*mid_list;
-	t_list	*mid_list_head;
-	char	*temp;;
-	char	*del;
+	t_strf	f;
 
-
-	ft_strtrisect(s, &head, &tail, &mid);
-	mid_list = split_commas(mid);
-	mid_list_head = mid_list;
-	while (mid_list)
+	ft_strtrisect(s, &(f.head), &(f.tail), &(f.mid));
+	f.mid_list = split_commas(f.mid);
+	f.mid_list_head = f.mid_list;
+	while (f.mid_list)
 	{
-		temp = ft_strjoin(head, mid_list->content);
-		del = temp;
-		temp = ft_strjoin(temp, tail);
-		ft_lst_add_last(list, ft_lst_new_ref(temp, sizeof(char *)));
-		ft_strdel(&del);
-		mid_list = mid_list->next;
+		f.temp = ft_strjoin(f.head, f.mid_list->content);
+		f.del = f.temp;
+		f.temp = ft_strjoin(f.temp, f.tail);
+		ft_lst_add_last(list, ft_lst_new_ref(f.temp, sizeof(char *)));
+		ft_strdel(&(f.del));
+		f.mid_list = f.mid_list->next;
 	}
-	ft_strdel(&head);
-	ft_strdel(&mid);
-	ft_strdel(&tail);
-	ft_lstdel(&mid_list_head, (void (*)(void *, size_t))free);
+	ft_strdel(&(f.head));
+	ft_strdel(&(f.mid));
+	ft_strdel(&(f.tail));
+	ft_lstdel(&(f.mid_list_head), (void (*)(void *, size_t))free);
 }
 
-t_list 			*ft_super_factory(char *s)
+t_list			*ft_super_factory(char *s)
 {
-	t_list *list = 0;
-	t_list *prev = 0;
-	t_list *next;
-	t_list *head;
-	char	*str;
+	t_sprf	f;
 
-	ft_lst_add_last(&list, ft_lstnew(s, ft_strlen(s) + 1));
-	head = list;
-	while (list)
+	ft_bzero(&f, sizeof(t_sprf));
+	ft_lst_add_last(&(f.list), ft_lstnew(s, ft_strlen(s) + 1));
+	f.head = f.list;
+	while (f.list)
 	{
-		str = list->content;
-		next = list->next;
-		if (find_closing(str, NULL) >= 0)
+		f.str = f.list->content;
+		f.next = f.list->next;
+		if (find_closing(f.str, NULL) >= 0)
 		{
-			ft_str_factory(str, &list);
-			ft_strdel(&str);
-			next = list->next;
-			if (prev)
-				prev->next = next;
+			ft_str_factory(f.str, &(f.list));
+			ft_strdel(&(f.str));
+			f.next = f.list->next;
+			if (f.prev)
+				f.prev->next = f.next;
 			else
-				head = next;
-			free(list);
+				f.head = f.next;
+			free(f.list);
 		}
 		else
-			prev = list;
-		list = next;
+			f.prev = f.list;
+		f.list = f.next;
 	}
-	return (head);
+	return (f.head);
 }
