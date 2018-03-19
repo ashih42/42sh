9c28bfa3d386c6b70832dc22f6fbcc80b1e815fa
nmei-42 <nmei@e1z2r9p35.42.us.org>
2018-03-18 19:21:18 -0700

    Freeing e->buffer at exit

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ashih <ashih@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nmei <nmei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 21:56:18 by ashih             #+#    #+#             */
-/*   Updated: 2018/03/18 06:07:45 by ashih            ###   ########.fr       */
+/*   Updated: 2018/03/18 19:20:13 by nmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void			ft_democracy(t_env *e)
 	ft_lstdel(&(e->children_pids), NULL);
 	ft_dl_lstdel(&(e->cmd_history), (void (*)(void *, size_t))free);
 	ft_lstdel(&(e->envp), (void (*)(void *, size_t))ft_char_array_del);
+	ft_strdel(&e->buffer);
 }
 
 int				ft_exit(t_env *e, int argc, char **argv)
